5317dc3f50667aae2ad4fab5eb5f7bb8a0251325
Gabriel Falcao Espindola <gabriel.espindola@sp.senai.br>
2024-02-21 12:47:50 -0300

    Inclusão de classe DataProcessing para separação de dados entre blocos, janelas e portas

--- TesteAlvenaria/Teste/DataProcessing.cs
+++ TesteAlvenaria/Teste/DataProcessing.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TesteAlvenaria.Teste
 {
@@ -10,15 +7,32 @@ namespace TesteAlvenaria.Teste
     {
         public void BlockOrOpening(string[] linesFile)
         {
-            List<string> resultado = new List<string>();
+            List<string> blocks = new List<string>();
+            List<string> windows = new List<string>();
+            List<string> doors = new List<string>();
 
-            foreach (string str in arrayDeStrings)
+            foreach (string str in linesFile)
             {
+                // Verifica se a string atual começa com "bloco" (insensível a maiúsculas e minúsculas)
+                if (str.StartsWith("bloco", StringComparison.OrdinalIgnoreCase))
+                {
+                    blocks.Add(str);
+                }
 
-                string[] partes = str.Split(' ');
+                if (str.StartsWith("Janela", StringComparison.OrdinalIgnoreCase))
+                {
+                    windows.Add(str);
+                }
 
-                resultado.AddRange(partes);
+                if (str.StartsWith("porta", StringComparison.OrdinalIgnoreCase))
+                {
+                    doors.Add(str);
+                }
             }
+
+            Console.WriteLine(blocks.ToArray());
+            Console.WriteLine(windows.ToArray());
+            Console.WriteLine(doors.ToArray());
         }
     }
 }
